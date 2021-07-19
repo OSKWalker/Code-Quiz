214f8a47ad71d637de62833248f710a9cbc4781a
OSKWalker <84692354+OSKWalker@users.noreply.github.com>
2021-07-18 23:09:03 -0400

    added interaction buttons and framework for interactive functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
-var timerEl = document.getElementById("timer");
-var startEl = document.getElementById("start");
-var quizAreaEl = document.getElementById("quizArea");
+const scoreBarEl = document.getElementById("scoreBar");
+const viewScoreButtonEl = document.getElementById("viewScoresButton");
+const timerSpaceEl = document.getElementById("timerSpace");
+const timerEl = document.getElementById("timer");
+const quizAreaEl = document.getElementById("quizArea");
+const resultsAreaEl = document.getElementById("resultsArea")
+const startButtonEl = document.getElementById("startButton");
+const scoreButtonEl = document.getElementById("scoreButton");
+const submitButtonEl = document.getElementById("submitButton");
+
 
 let secondsLeft = 0;
 
@@ -25,6 +32,7 @@ function countDown() {
 function sendMessage() {
     timerEl.textContent = "";
     var gameOver = document.createElement("h1");
+    gameOver.textContent = "GAME OVER!";
     gameOver.setAttribute("style", "font-weight: bold");
     quizAreaEl.appendChild(gameOver);
   
@@ -38,4 +46,54 @@ function sendMessage() {
 
   }
 
-  startEl.addEventListener("click", startQuiz);
+// Function to view scores
+function viewScores(){}
+
+// Function to hide scores
+function hideScores(){};
+
+// Function to show scores
+function showScores(){
+
+    viewScores();
+};
+
+// Function to save scores
+function saveScores(){
+
+    hideScores();
+
+};
+/*
+// Capture contents of localstorage
+
+let savedScores = JSON.parse(localStorage.getItem("scoreHistory"));
+if(savedScores == null)
+{
+    savedScores = [];
+}
+
+// Saves the Score
+
+//save score when prompted
+function saveScore(event){
+    event.preventDefault();
+    var userScore = {
+        userInitial: document.querySelector("#user-initials").value.trim(),
+        userScore: secondsLeft
+    };
+    savedScores.push(userScore);
+    localStorage.setItem("scoreHistory",JSON.stringify(savedScores));
+}*/
+
+// Click event to view high scores
+viewScoreButtonEl.addEventListener("click", viewScores);
+
+// Click event to start the quiz
+startButtonEl.addEventListener("click", startQuiz);
+
+// Click event to show high scores and space to save initials and score
+scoreButtonEl.addEventListener("click", showScores);
+
+// Click event to save initials and score
+submitButtonEl.addEventListener("click", saveScores);
