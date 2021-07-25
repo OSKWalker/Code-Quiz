cd643d23520cf55c3a5d8481056d86ac5107b268
OSKWalker <84692354+OSKWalker@users.noreply.github.com>
2021-07-25 14:06:14 -0400

    altered styling, saving progress on functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,237 +4,208 @@ const timerSpaceEl = document.getElementById("timerSpace");
 const timerEl = document.getElementById("timer");
 const quizTitleEl = document.getElementById("quizTitle");
 const quizAreaEl = document.getElementById("quizArea");
-const resultsAreaEl = document.getElementById("resultsArea")
+const resultsAreaEl = document.getElementById("resultsArea");
+const highScoresAreaEl = document.getElementById("highScoresArea");
+const highScoresTableEl = document.getElementById("highScoresTable");
+const tableBodyEl = document.getElementById("table-body");
 const startButtonEl = document.getElementById("startButton");
 const scoreButtonEl = document.getElementById("scoreButton");
 const submitButtonEl = document.getElementById("submitButton");
 const quizQuestions = [
-    
-    {
-        question: "Question 1?",
-        
-        answers: {
-            
-            A: "Answer A",
-            B: "Answer B",
-            C: "Answer C",
-            D: "Answer D"
-        },
+  {
+    question: "Question 1?",
 
-        correctAnswer: "Answer A"
-    
+    answers: {
+      A: "Answer A",
+      B: "Answer B",
+      C: "Answer C",
+      D: "Answer D",
     },
 
-    {
-        question: "Question 2?",
-        
-        answers: {
-            
-            A: "Answer A",
-            B: "Answer B",
-            C: "Answer C",
-            D: "Answer D"
-        },
+    correctAnswer: "Answer A",
+  },
 
-        correctAnswer: "Answer B"
-    
+  {
+    question: "Question 2?",
+
+    answers: {
+      A: "Answer A",
+      B: "Answer B",
+      C: "Answer C",
+      D: "Answer D",
     },
 
-    {
-        question: "Question 3?",
-        
-        answers: {
-            
-            A: "Answer A",
-            B: "Answer B",
-            C: "Answer C",
-            D: "Answer D"
-        },
+    correctAnswer: "Answer B",
+  },
 
-        correctAnswer: "Answer B"
-    
+  {
+    question: "Question 3?",
+
+    answers: {
+      A: "Answer A",
+      B: "Answer B",
+      C: "Answer C",
+      D: "Answer D",
     },
 
-    {
-        question: "Question 4?",
-        
-        answers: {
-            
-            A: "Answer A",
-            B: "Answer B",
-            C: "Answer C",
-            D: "Answer D"
-        },
+    correctAnswer: "Answer B",
+  },
 
-        correctAnswer: "Answer A"
-    
+  {
+    question: "Question 4?",
+
+    answers: {
+      A: "Answer A",
+      B: "Answer B",
+      C: "Answer C",
+      D: "Answer D",
     },
 
-    {
-        question: "Question 5?",
-        
-        answers: {
-            
-            A: "Answer A",
-            B: "Answer B",
-            C: "Answer C",
-            D: "Answer D"
-        },
+    correctAnswer: "Answer A",
+  },
 
-        correctAnswer: "Answer C"
-    
+  {
+    question: "Question 5?",
+
+    answers: {
+      A: "Answer A",
+      B: "Answer B",
+      C: "Answer C",
+      D: "Answer D",
     },
 
-    {
-        question: "Question 6?",
-        
-        answers: {
-            
-            A: "Answer A",
-            B: "Answer B",
-            C: "Answer C",
-            D: "Answer D"
-        },
+    correctAnswer: "Answer C",
+  },
 
-        correctAnswer: "Answer A"
-    
+  {
+    question: "Question 6?",
+
+    answers: {
+      A: "Answer A",
+      B: "Answer B",
+      C: "Answer C",
+      D: "Answer D",
     },
 
-    {
-        question: "Question 7?",
-        
-        answers: {
-            
-            A: "Answer A",
-            B: "Answer B",
-            C: "Answer C",
-            D: "Answer D"
-        },
+    correctAnswer: "Answer A",
+  },
 
-        correctAnswer: "Answer D"
-    
+  {
+    question: "Question 7?",
+
+    answers: {
+      A: "Answer A",
+      B: "Answer B",
+      C: "Answer C",
+      D: "Answer D",
     },
 
-    {
-        question: "Question 8?",
-        
-        answers: {
-            
-            A: "Answer A",
-            B: "Answer B",
-            C: "Answer C",
-            D: "Answer D"
-        },
+    correctAnswer: "Answer D",
+  },
 
-        correctAnswer: "Answer A"
-    
+  {
+    question: "Question 8?",
+
+    answers: {
+      A: "Answer A",
+      B: "Answer B",
+      C: "Answer C",
+      D: "Answer D",
     },
 
-    {
-        question: "Question 9?",
-        
-        answers: {
-            
-            A: "Answer A",
-            B: "Answer B",
-            C: "Answer C",
-            D: "Answer D"
-        },
+    correctAnswer: "Answer A",
+  },
 
-        correctAnswer: "Answer B"
-    
+  {
+    question: "Question 9?",
+
+    answers: {
+      A: "Answer A",
+      B: "Answer B",
+      C: "Answer C",
+      D: "Answer D",
     },
 
-    {
-        question: "Question 10?",
-        
-        answers: {
-            
-            A: "Answer A",
-            B: "Answer B",
-            C: "Answer C",
-            D: "Answer D"
-        },
+    correctAnswer: "Answer B",
+  },
 
-        correctAnswer: "Answer B"
-    
+  {
+    question: "Question 10?",
+
+    answers: {
+      A: "Answer A",
+      B: "Answer B",
+      C: "Answer C",
+      D: "Answer D",
     },
 
-    {
-        question: "Question 11?",
-        
-        answers: {
-            
-            A: "Answer A",
-            B: "Answer B",
-            C: "Answer C",
-            D: "Answer D"
-        },
+    correctAnswer: "Answer B",
+  },
 
-        correctAnswer: "Answer A"
-    
+  {
+    question: "Question 11?",
+
+    answers: {
+      A: "Answer A",
+      B: "Answer B",
+      C: "Answer C",
+      D: "Answer D",
     },
 
-    {
-        question: "Question 12?",
-        
-        answers: {
-            
-            A: "Answer A",
-            B: "Answer B",
-            C: "Answer C",
-            D: "Answer D"
-        },
+    correctAnswer: "Answer A",
+  },
 
-        correctAnswer: "Answer D"
-    
+  {
+    question: "Question 12?",
+
+    answers: {
+      A: "Answer A",
+      B: "Answer B",
+      C: "Answer C",
+      D: "Answer D",
     },
 
-    {
-        question: "Question 13?",
-        
-        answers: {
-            
-            A: "Answer A",
-            B: "Answer B",
-            C: "Answer C",
-            D: "Answer D"
-        },
+    correctAnswer: "Answer D",
+  },
 
-        correctAnswer: "Answer B"
-    
+  {
+    question: "Question 13?",
+
+    answers: {
+      A: "Answer A",
+      B: "Answer B",
+      C: "Answer C",
+      D: "Answer D",
     },
 
-    {
-        question: "Question 14?",
-        
-        answers: {
-            
-            A: "Answer A",
-            B: "Answer B",
-            C: "Answer C",
-            D: "Answer D"
-        },
+    correctAnswer: "Answer B",
+  },
 
-        correctAnswer: "Answer A"
-    
+  {
+    question: "Question 14?",
+
+    answers: {
+      A: "Answer A",
+      B: "Answer B",
+      C: "Answer C",
+      D: "Answer D",
     },
 
-    {
-        question: "Question 15?",
-        
-        answers: {
-            
-            A: "Answer A",
-            B: "Answer B",
-            C: "Answer C",
-            D: "Answer D"
-        },
+    correctAnswer: "Answer A",
+  },
 
-        correctAnswer: "Answer C"
-    
-    }
+  {
+    question: "Question 15?",
 
+    answers: {
+      A: "Answer A",
+      B: "Answer B",
+      C: "Answer C",
+      D: "Answer D",
+    },
+
+    correctAnswer: "Answer C",
+  },
 ];
 
 let highScores = [];
@@ -245,236 +216,253 @@ let thisQuestion = "";
 let theseAnswers = null;
 let answerKey = [];
 
+// Control the timer
 function countDown() {
-    // Sets interval in variable
-    var timerInterval = setInterval(function(){
-      secondsLeft--;
-      timerEl.innerText = secondsLeft;
-  
-      if(secondsLeft <= 0) {
+  // Sets interval in variable
+  timerInterval = setInterval(function () {
+    secondsLeft--;
+    timerEl.innerText = secondsLeft;
 
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-
-        // Calls function to create and append image
-        endGame();
-      }
-  
-    }, 1000);
-  }
-
-// Function to create and append "GAME OVER! message"
-function endGame() {
-    timerEl.textContent = "";
-    quizAreaEl.innerHTML = "";
-    var gameOver = document.createElement("h1");
-    gameOver.textContent = "GAME OVER!";
-    gameOver.setAttribute("style", "font-weight: bold");
-    quizAreaEl.appendChild(gameOver);
-    showScoreButton();
-    return;
-  
-  }
-// Function to initiate the quiz and start the timer
-function startQuiz(){
-    
-    hideStartButton();
-    hideQuizTitle();
-    viewScoreBar();
-    secondsLeft = 5;
-    countDown();
-    questionIndex = 0;        
-    getQuestion();    
-
+    if (secondsLeft <= 0) {
+      // Calls function to create and append image
+      endGame();
+    }
+  }, 1000);
 }
 
-function showTime(){
+// "GAME OVER!"
+function endGame() {
+  timerEl.textContent = "";
+  clearInterval(timerInterval);
+  quizAreaEl.innerHTML = "";
+  var gameOver = document.createElement("h1");
+  gameOver.textContent = "GAME OVER!";
+  gameOver.setAttribute("style", "font-weight: bold");
+  quizAreaEl.appendChild(gameOver);
+  showScoreButton();
+  return;
+}
 
-    timerSpaceEl.style.display = "initial";
+// Initiate the quiz and start the timer
+function startQuiz() {
+  hideStartButton();
 
-};
+  hideQuizTitle();
 
-function hideTime(){
+  viewScoreBar();
 
-    timerSpaceEl.style.display = "none";
-    
-};
+  secondsLeft = 5;
 
-function showQuizTitle(){
+  countDown();
 
-    quizTitleEl.style.display = "initial";
+  questionIndex = 0;
 
-};
+  getQuestion();
+}
 
-function hideQuizTitle(){
+// Display the timer
+function showTime() {
+  timerSpaceEl.style.display = "initial";
+}
 
-    quizTitleEl.style.display = "none";
+// Hide the timer
+function hideTime() {
+  timerSpaceEl.style.display = "none";
+}
 
-};
+// Display the quiz title
+function showQuizTitle() {
+  quizTitleEl.style.display = "initial";
+}
 
-function showStartButton(){
+// Hide the quiz title
+function hideQuizTitle() {
+  quizTitleEl.style.display = "none";
+}
 
-    startButtonEl.style.display = "initial";
+// Display quiz area
+function showQuizArea() {
+  quizAreaEl.style.display = "initial";
+}
 
-};
+// Hide quiz area
+function hideQuizArea() {
+  quizAreaEl.style.display = "none";
+}
 
-function hideStartButton(){
+// Display the start button
+function showStartButton() {
+  startButtonEl.style.display = "initial";
+}
 
-    startButtonEl.style.display = "none";
-    
-};
+// Hide the start button
+function hideStartButton() {
+  startButtonEl.style.display = "none";
+}
 
-function showScoreButton(){
+// Display the score button
+function showScoreButton() {
+  scoreButtonEl.style.display = "initial";
+}
 
-    scoreButtonEl.style.display = "initial";
+// Hide the score button
+function hideScoreButton() {
+  scoreButtonEl.style.display = "none";
+}
 
-};
+//Display the submit button
+function showSubmitButton() {
+  submitButtonEl.style.display = "initial";
+}
 
-function hideScoreButton(){
+// Hide the submit button
+function hideSubmitButton() {
+  submitButtonEl.style.display = "none";
+}
 
-    scoreButtonEl.style.display = "none";
-    
-};
+// Generate a question
+function getQuestion() {
+  // Clears the quiz space
+  quizAreaEl.innerHTML = "";
 
-function showSubmitButton(){
+  // Displays the question
+  var questionSpace = document.createElement("h1");
+  questionSpace.innerHTML = quizQuestions[questionIndex].question;
+  quizAreaEl.appendChild(questionSpace);
 
-    submitButtonEl.style.display = "initial";
+  // Displays the answer choices
+  theseAnswers = quizQuestions[questionIndex].answers;
+  answerKey = Object.keys(theseAnswers);
 
-};
+  for (let i = 0; i < answerKey.length; i++) {
+    let answerIndex = answerKey[i];
+    var answerSpace = document.createElement("ul");
+    var choice = document.createElement("li");
+    var selection = document.createElement("button");
 
-function hideSubmitButton(){
+    selection.addEventListener("click", function (event) {
+      event.preventDefault();
 
-    submitButtonEl.style.display = "none";
-    
-};
+      event.stopPropagation();
 
-function getQuestion(){
-    
-    // Clears the quiz space
-    quizAreaEl.innerHTML = "";
+      let selected = event.target;
 
-    // Ends game if there are no more questions
-    if(questionIndex>=quizQuestions.length){
+      if (selected.matches("button") === true) {
+        var userSelection = selected.innerText;
 
-        endGame();
-    
-    };
-
-    // Displays the question
-    var questionSpace = document.createElement("h1");
-    questionSpace.innerHTML = quizQuestions[questionIndex].question;
-    quizAreaEl.appendChild(questionSpace);
-    theseAnswers = quizQuestions[questionIndex].answers;
-
-    answerKey = Object.keys(theseAnswers);
-
-    // Displays the answer choices    
-    for(let i=0; i < answerKey.length; i++){
-        
-        let answerIndex = answerKey[i];
-        var answerSpace = document.createElement("ul");
-        var choice = document.createElement("li");
-        var selection = document.createElement("button");
-        selection.addEventListener('click', function(event){
-
-            event.preventDefault();
-            event.stopPropagation();
-
-        let selected = event.target;
-        
-        if(selected.matches("button")===true){
-
-            var userSelection = selected.innerText;
-
-            checkAnswer(userSelection);
-
-        }else{
-
-            return;
-        
-        };
+        checkAnswer(userSelection);
+      } else {
+        return;
+      }
     });
-        choice.innerText = answerIndex + ": ";
-        selection.innerText = theseAnswers[answerIndex];
-        choice.appendChild(selection);
-        answerSpace.appendChild(choice);
-        quizAreaEl.appendChild(answerSpace);
-    };
 
-};
+    choice.innerText = answerIndex + ": ";
+    selection.innerText = theseAnswers[answerIndex];
+    choice.appendChild(selection);
+    answerSpace.appendChild(choice);
+    quizAreaEl.appendChild(answerSpace);
+  }
+}
 
-// Checks for correct answer
-function checkAnswer(userSelection){    
-    
-    var correct = quizQuestions[questionIndex].correctAnswer;
+// Check for correct answer; Take time if incorrect, Add time if correct
+function checkAnswer(userSelection) {
+  var correct = quizQuestions[questionIndex].correctAnswer;
+  var userAlert = document.createElement("h1");
 
-    if (userSelection !== correct){
+  if (userSelection !== correct) {
+    userAlert.textContent = "INCORRECT!";
+
+    secondsLeft -= 4;
+  } else {
+    userAlert.textContent = "CORRECT!";
+
+    secondsLeft += 2;
+  }
+
+  userAlert.setAttribute(
+    "style",
+    "font-size: 48px font-weight: bold font-style: italics"
+  );
+
+  resultsAreaEl.appendChild(userAlert);
+
+  /*
+
+    setTimeout(() => {
+
+        quizAreaEl.la
         
-        console.log("wrong");
-        secondsLeft-=4;
-    
-    }else{
-        
-        console.log("correct");
-        secondsLeft+=2;
-    
-    };
+    }, 500);*/
 
-    nextQuestion();
-
+  nextQuestion();
 }
 
 // Generates next question
-function nextQuestion(){
+function nextQuestion() {
+  while (quizAreaEl.firstChild) {
+    quizAreaEl.removeChild(quizAreaEl.firstChild);
+  }
 
-    questionIndex++;
-    
-    if(questionIndex>=quizQuestions.length || secondsLeft <= 0){
+  questionIndex++;
 
-        endGame();
-
-    }else{
-        
-        getQuestion();
-
-    }
-
-    
-
-};
+  if (questionIndex >= quizQuestions.length || secondsLeft <= 0) {
+    endGame();
+  } else {
+    getQuestion();
+  }
+}
 
 // Toggle high score viewing
-function viewScores(){};
+function viewScores() {
+  var arr = [
+    { userInitials: "AAA", userScore: 48 },
+
+    { userInitials: "MMM", userScore: 36 },
+
+    { userInitials: "ZZZ", userScore: 24 },
+  ];
+
+  tableBodyEl.innerHTML = "";
+
+  arr.forEach(function (score) {
+    var tRow = document.createElement("tr");
+    var tContent = `<td>${score.userInitials}</td><td>${score.userScore}</td>`;
+    tRow.innerHTML = tContent;
+    tableBodyEl.appendChild(tRow);
+  });
+
+  //highScoresTableEl
+}
 
 // Function to view scorebar
-function viewScoreBar(){
-
-    scoreBarEl.style.display = "initial";
-
-};
+function viewScoreBar() {
+  scoreBarEl.style.display = "initial";
+}
 
 // Function to hide scorebar
-function hideScoreBar(){  
-    
-    scoreBarEl.style.display = "none";
-
-};
+function hideScoreBar() {
+  scoreBarEl.style.display = "none";
+}
 
 // Function to show scores
-function showScores(){
+function showScores() {
+  hideTime();
 
-    hideScoreBar();
+  hideScoreButton();
 
-    hideScoreButton();
+  hideQuizArea();
+  /*
+    var scoreText = document.createElement("h1");
+    scoreText.innerText = "YOUR SCORE IS: " + secondsLeft + "!";
+    scoreText.setAttribute("style", "font-weight: bold");
+    resultsAreaEl.appendChild(scoreText);*/
 
-    showSubmitButton();
-
-};
+  showSubmitButton();
+}
 
 // Function to save scores
-function saveScores(){
-
-    /*
+function saveScores() {
+  /*
 // Capture contents of localstorage
 
 let savedScores = JSON.parse(localStorage.getItem("scoreHistory"));
@@ -496,53 +484,42 @@ function saveScore(event){
     localStorage.setItem("scoreHistory", JSON.stringify(savedScores));
 }*/
 
-    init();
-
-};
+  init();
+}
 
 // Adds requisite event listeners
-function setEventListeners(){
+function setEventListeners() {
+  // Click event to view high scores
+  viewScoreButtonEl.addEventListener("click", viewScores);
 
-// Click event to view high scores
-viewScoreButtonEl.addEventListener("click", viewScores);
+  // Click event to start the quiz
+  startButtonEl.addEventListener("click", startQuiz);
 
-// Click event to start the quiz
-startButtonEl.addEventListener("click", startQuiz);
+  // Click event to show high scores and space to save initials and score
+  scoreButtonEl.addEventListener("click", showScores);
 
-// Click event to show high scores and space to save initials and score
-scoreButtonEl.addEventListener("click", showScores);
-
-// Click event to save initials and score
-submitButtonEl.addEventListener("click", saveScores);
-
-};
+  // Click event to save initials and score
+  submitButtonEl.addEventListener("click", saveScores);
+}
 
 // Initializing function
-function init(){
+function init() {
+  hideTime();
 
-    if(highScores===[]){
-        
-        hideTime();
+  showQuizTitle();
 
-    }else{
-        
-        hideScoreBar();
+  showStartButton();
 
-    }
+  hideScoreButton();
 
-    showQuizTitle();
+  hideSubmitButton();
 
-    showStartButton();
-    
-    hideScoreButton();
+  setEventListeners();
 
-    hideSubmitButton();
-
-    setEventListeners();
-
-    quizAreaEl.textContent = "ARE YOU READY TO TEST YOUR KNOWLEDGE OF GENERAL CODING TRIVIA?"
-
-};
+  quizTitleEl.textContent = "Code Quiz Challenge";
+  quizAreaEl.textContent =
+    "ARE YOU READY TO TEST YOUR KNOWLEDGE OF GENERAL CODING TRIVIA?";
+}
 
 // Execute the initializing function
 init();
